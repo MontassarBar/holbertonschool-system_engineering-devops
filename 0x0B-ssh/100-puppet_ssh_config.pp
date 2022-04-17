f5a5fa2eaa14ccc8a077a9f7dd6ac3210bf6381c
MontassarBar <montassarbarbouchi7@gmail.com>
2022-04-18 00:51:26 +0200

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 #set up my client SSH configuration file so that I can connect to a server without typing a password
 
 file {'ssh_config'
-ensure => 'present',
+ensure => present,
 path  => '/etc/ssh/sshd_config',
 line  => 'HostbasedAuthentication no',
 }
 file {'ssh config'
-ensure => 'present',
+ensure => present,
 path  => '/etc/ssh/sshd_config',
 line  => 'IdentityFile ~/.ssh/school'
 }
