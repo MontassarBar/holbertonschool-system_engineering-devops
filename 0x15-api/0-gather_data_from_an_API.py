dc48346b4c9a3adafa959f590b9454530fa1f220
MontassarBar <montassarbarbouchi7@gmail.com>
2022-05-16 21:12:28 +0200

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 '''Python script that, using a REST API, for a given employee ID,
 returns information about his/her TODO list progress'''
-from sys import argv
 from requests import get
+from sys import argv
 if __name__ == "__main__":
     URL = "https://jsonplaceholder.typicode.com/todos/"
     URL2 = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
