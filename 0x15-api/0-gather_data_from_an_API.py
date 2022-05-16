ad2e6aeb1e192bfcefa7fc6290e2339c08a81b68
MontassarBar <montassarbarbouchi7@gmail.com>
2022-05-16 21:11:49 +0200

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-from requests import get
+'''Python script that, using a REST API, for a given employee ID,
+returns information about his/her TODO list progress'''
 from sys import argv
+from requests import get
 if __name__ == "__main__":
     URL = "https://jsonplaceholder.typicode.com/todos/"
     URL2 = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
@@ -14,9 +16,10 @@ if __name__ == "__main__":
     for i in range(0, len(response)):
         if response[i]["userId"] == int(argv[1]):
             c += 1
-            if response[i]["completed"] == True:
+            if response[i]["completed"] is True:
                 v += 1
     print("Employee {} is done with tasks({}/{}):".format(name, v, c))
     for i in range(0, len(response)):
-        if response[i]["userId"] == int(argv[1]) and response[i]["completed"] == True:
-            print("     ",response[i]["title"])
+        if response[i]["userId"] == int(argv[1]) and response[i][
+                "completed"] is True:
+            print("     ", response[i]["title"])
