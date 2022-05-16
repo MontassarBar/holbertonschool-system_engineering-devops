60dae0ee63d2ae0ee9965a6a5ba6a3e9996dd805
MontassarBar <montassarbarbouchi7@gmail.com>
2022-05-17 00:22:04 +0200

    Export to JSON

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,19 +7,19 @@ if __name__ == "__main__":
     URL2 = "https://jsonplaceholder.typicode.com/users/"
     x = get(URL)
     y = get(URL2)
-    l = []
     dict2 = {}
     file = "todo_all_employees.json"
     response = x.json()
     response2 = y.json()
-    for j in range(1, len(response2)):
+    for j in range(0, len(response2)):
+        l = []
         for i in range(0, len(response)):
-            if response[i]["userId"] == j:
+            if response[i]["userId"] == j + 1:
                 dict = {}
-                dict["username"] = response2[j - 1]["username"]
+                dict["username"] = response2[j]["username"]
                 dict["task"] = response[i]["title"]
                 dict["completed"] = response[i]["completed"]
                 l.append(dict)
-        dict2[j] = l
+        dict2[j + 1] = l
     with open(file, mode="w") as x:
         json.dump(dict2, x)
